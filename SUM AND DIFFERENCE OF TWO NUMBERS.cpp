370c6232625fd3fce721d5ad8b0ea2221e73c870
naveenarepaka2-del <naveenarepaka2@gmail.com>
2025-12-11 12:23:08 +0530

    Add files via upload

---
+++ SUM AND DIFFERENCE OF TWO NUMBERS.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    float x, y;
+
+    // Read two integers
+    scanf("%d %d", &a, &b);
+
+    // Read two floats
+    scanf("%f %f", &x, &y);
+
+    // Print integer sum and difference
+    printf("%d %d\n", a + b, a - b);
+
+    // Print float sum and difference (rounded to 1 decimal place)
+    printf("%.1f %.1f\n", x + y, x - y);
+
+    return 0;
+}
+
